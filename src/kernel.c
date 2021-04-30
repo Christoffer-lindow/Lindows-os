313192582f883914e6e5b059c52c389f64f4606b
Christoffer-lindow <clindow@gmail.com>
2021-04-30 16:59:55 +0700

    Added virtual memory paging

--- src/kernel.c
+++ src/kernel.c
@@ -5,7 +5,7 @@
 #include "idt/idt.h"
 #include "io/io.h"
 #include "memory/heap/kheap.h"
-
+#include "memory/paging/paging.h"
 
 uint16_t *video_mem = 0;
 uint16_t terminal_row = 0;
@@ -73,7 +73,7 @@ void print(const char *str)
     }
 }
 
-
+static struct paging_4gb_chunk* kernel_chunk = 0;
 void kernel_main()
 {
     terminal_initialize();
@@ -85,6 +85,17 @@ void kernel_main()
     // Initialize interuppts
     idt_init();
 
+    // Setup paging
+    kernel_chunk = paging_new_4gb(PAGING_IS_WRITABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
+
+    // Switch to kernel paging chunk
+    paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
+
+    char* ptr = kzalloc(PAGING_PAGE_SIZE);
+    paging_set(paging_4gb_chunk_get_directory(kernel_chunk),(void*)0x1000,(uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT | PAGING_IS_WRITABLE);
+
+    // Enable paging
+    enable_paging();
     // Enable interrupts
     enable_interrupts();
 
