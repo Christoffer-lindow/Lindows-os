cdb59415a2ee883642fa80c23a771dca5be2bfc5
Christoffer-lindow <clindow@gmail.com>
2021-05-02 20:35:24 +0700

    Implemented fclose

--- src/fs/fat/fat16.c
+++ src/fs/fat/fat16.c
@@ -121,14 +121,17 @@ int fat16_resolve(struct disk *disk);
 void *fat16_open(struct disk *disk, struct path_part *path, FILE_MODE mode);
 int fat16_read(struct disk *disk, void *descriptor, uint32_t size, uint32_t nmemb, char *out_ptr);
 int fat16_seek(void *private, uint32_t offset, FILE_SEEK_MODE seek_mode);
-int fat16_stat(struct disk* disk, void* private, struct file_stat* stat);
+int fat16_stat(struct disk *disk, void *private, struct file_stat *stat);
+int fat16_close(void *private);
+
 struct filesystem fat16_fs =
     {
         .resolve = fat16_resolve,
         .open = fat16_open,
         .read = fat16_read,
         .seek = fat16_seek,
-        .stat = fat16_stat};
+        .stat = fat16_stat,
+        .close = fat16_close};
 
 struct filesystem *fat16_init()
 {
@@ -616,18 +619,28 @@ void *fat16_open(struct disk *disk, struct path_part *path, FILE_MODE mode)
 
     return descriptor;
 }
+static void fat16_free_file_descriptor(struct fat_file_descriptor *desc)
+{
+    fat16_fat_free_item(desc->item);
+    kfree(desc);
+}
 
-int fat16_stat(struct disk* disk, void* private, struct file_stat* stat)
+int fat16_close(void *private)
+{
+    fat16_free_file_descriptor((struct fat_file_descriptor *)private);
+    return 0;
+}
+int fat16_stat(struct disk *disk, void *private, struct file_stat *stat)
 {
     int res = 0;
-    struct fat_file_descriptor* descriptor = (struct fat_file_descriptor*) private;
-    struct fat_item* desc_item = descriptor->item;
-    if(desc_item->type != FAT_ITEM_TYPE_FILE)
+    struct fat_file_descriptor *descriptor = (struct fat_file_descriptor *)private;
+    struct fat_item *desc_item = descriptor->item;
+    if (desc_item->type != FAT_ITEM_TYPE_FILE)
     {
         res = -EINVARG;
         goto out;
     }
-    struct fat_directory_item* ritem = desc_item->item;
+    struct fat_directory_item *ritem = desc_item->item;
     stat->filesize = ritem->filesize;
     stat->flags = 0x00;
 
